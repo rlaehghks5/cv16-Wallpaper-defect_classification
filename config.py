74726be89480a500c2b8a799ccf3ada025280bf4
rlaehghks5 <rlaehghks5@gmail.com>
2023-04-27 07:09:36 +0000

    dohwan3

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 CFG = {
     'IMG_SIZE':300,
-    'EPOCHS':1,
-    'LEARNING_RATE':1e-4,
+    'EPOCHS':30,
+    'LEARNING_RATE':1e-3,
     'BATCH_SIZE':32,
     'SEED':41,
     'MODEL': 'efficientnet_b3',
     'OPTIMIZER' : 'AdamW',
     'CRITERION' : 'cross_entropy',
     'NAME' : 'exp',
-    'PATIENCE' : 8,
+    'PATIENCE' : 5,
     'CLASSES' : 19
     }
