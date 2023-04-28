4fae2052d3d6ae21272e8ed69cafdd4d5e320908
Kim Geun Uk <74355042+KimGeunUk@users.noreply.github.com>
2023-04-28 10:52:41 +0900

    Delete set_path.py

--- utils_/set_path.py
+++
@@ -1,9 +0,0 @@
-TRAIN_IMG_FOLDER_PATH = "/opt/ml/open/Data/Training_whole/PNG"
-TEST_IMG_PATH = "/opt/ml/open"
-
-TRAIN_CSV_PATH = "/opt/ml/cv16-Wallpaper-defect_classification/data/train.csv"
-TEST_CSV_PATH = "/opt/ml/cv16-Wallpaper-defect_classification/data/test.csv"
-SUBMIT_CSV_PATH = "/opt/ml/cv16-Wallpaper-defect_classification/data/sample_submission.csv"
-
-MODEL_SAVE_PATH = "/opt/ml/cv16-Wallpaper-defect_classification/models"
-SUBMIT_SAVE_PATH = "/opt/ml/cv16-Wallpaper-defect_classification/submits"
